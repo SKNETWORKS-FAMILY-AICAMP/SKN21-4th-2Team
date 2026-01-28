36f373004c662c14c9addc7418d8c9f7cab7fd60
ksryong0 <ksryong0@gmail.com>
2026-01-29 00:29:38 +0900

    1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,14 +104,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "account.CustomUser"
+#######################################
+LOGIN_URL = '/account/login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
