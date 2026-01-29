26be6d336d39802647e41e5e9637112601506803
Jung Deokkyu <duck213@gmail.com>
2026-01-29 10:49:36 +0900

    backend app 추가
    backend 회원,chat 수정
    
    Update req

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,6 +3,19 @@ from django.urls import path
 from . import views
 
 app_name = "account"
+
 urlpatterns = [
-    path("welcome", views.welcome, name="welcome")
+    path("welcome", views.welcome, name="welcome"),
+    path("create", views.create, name="create"),
+    path("detail", views.detail, name='detail'),
+    path("login", views.user_login, name="login"),
+    path("logout", views.user_logout, name="logout"),
+    path("update", views.update, name="update"),
+    path("password_change", views.password_change, name="password_change"),
+    path("delete", views.user_delete, name="delete"),
 ]
+
+from django.urls import path
+
+
+
--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "chat"
 urlpatterns = [
-    path("welcome", views.welcome, name="welcome")
+    path("welcome", views.welcome, name="welcome"),
+    path("chatting", views.chatting.as_view(), name="chatting"),
+    path("chat_session", views.chat_session.as_view(), name="chat_session")
 ]
