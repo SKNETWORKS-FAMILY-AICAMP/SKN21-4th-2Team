a301518770fa60bdce5ec3ead8b5db851ba6470c
ksryong0 <ksryong0@gmail.com>
2026-01-29 10:12:07 +0900

    1

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,6 +1,7 @@
 # polls/models.py
 #  모델 클래스들을 정의
 from django.db import models
+from account.models import CustomUser
 import uuid
 
 # 모델 클래스 정의 - Question(설문질문) - Choice(설문의 보기)
@@ -9,18 +10,21 @@ import uuid
 #                     Field == DB column, Model객체의 Instance 변수 이 둘에 대한 설정
 
 # Model class 정의 할 때 primary key Field를 선언하지 않으면, 
-# id (int auto_increment) 컬럼이 primary key 컬럼으로 자동으로 생성된다.
 class Chat(models.Model):
     # Field 정의: 변수명-(instance변수명, column 이름)
     #             Field 객체를 할당. Field객체 - column 설정(type, null허용여부,..)
-    user_id = models.IntegerField(default=0)  #CharField() -> 문자열타입(varchar, str)
+    username = models.ForeignKey(
+        CustomUser, # 참조할 Model Class
+        on_delete=models.CASCADE # 참조 값이 삭제 된 경우 어떻게 할지 -> cascade: 삭제
+    ) #   
     session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_active = models.BooleanField(default=True)
     # DateTimeField: 일시타입(datetime, datetime.datetime)
     # auto_now_add: insert 될 때 일시를 자동 입력.
-    
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f"{self.user_id}. {self.nick_name}"
+        return f"{self.username}. {self.session_id}. {self.is_active}. {self.created_at}"
 
     # create table question(
     #     question_text varchar(200),
@@ -28,18 +32,20 @@ class Chat(models.Model):
     # )
 
 # 보기 테이블
-class Choice(models.Model):
+class Chat_Message(models.Model):
 
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) # 정수타입 (int, int)
-    question = models.ForeignKey(
+    message_id = models.AutoField(primary_key=True)
+    role = models.CharField(max_length=10) # 문자열타입 (user 또는 assistant)
+    message = models.TextField() # 긴 문자열 타입
+    session_id = models.ForeignKey(
         Chat, # 참조할 Model Class
         on_delete=models.CASCADE # 참조 값이 삭제 된 경우 어떻게 할지 -> cascade: 삭제
         # , related_name="my_choice" # q.my_choice.all()
-    ) # FK -> Question의 id를 참조.
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.id}. {self.choice_text}"
+        return f"{self.message_id}. {self.role}. {self.message}. {self.session_id}. {self.created_at}"
 
 # create table choice(
 #     choice_text varchar(200) not null,
