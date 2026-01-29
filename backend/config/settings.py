76a01a026513fc074ba8c7dde78b623f3719b13a
ksryong0 <ksryong0@gmail.com>
2026-01-29 12:13:37 +0000

    1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '43.201.89.96']
 #public IP: 45.201.89.96
 
 # Application definition
